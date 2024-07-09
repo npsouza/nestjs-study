a24d656814e9cc4b9feddc2a50f360ca31f4eb2d
Nathan <nathanpereirasouza@gmail.com>
2024-07-09 22:18:27 +0000

    feat: import modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CategoriesModule } from './categories/categories.module';
+import { DatabaseModule } from './database/database.module';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [],
+  imports: [CategoriesModule, DatabaseModule, ProductsModule],
   controllers: [AppController],
   providers: [AppService],
 })
